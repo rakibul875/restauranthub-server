c1ba43aaf5e0d4b546c23327200f610e7a2adbd1
rakibul875 <rakibulislamashik78@gmail.com>
2026-07-13 13:56:48 +0600

    implement MongoDB connection and user/item routes

--- index.ts
+++ index.ts
@@ -5,19 +5,63 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error("MONGODB_URI is not defined in .env");
+}
 
-// Middlewares
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Home Route
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("restauranthub");
+    const userCollection = database.collection("user");
+    const itemCollection = database.collection("items");
+
+
+
+
+    app.get("/users", async (req: Request, res: Response) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    app.post('/items',async(req:Request,res:Response)=>{
+      const item = req.body;
+      const newItem = {
+        ...item,
+        createdAt: new Date()
+      }
+      const result = await itemCollection.insertOne(newItem);
+      res.send(result);
+    })
+
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 app.get("/", (req: Request, res: Response) => {
   res.send("RestaurantHub Server is Running...");
 });
 
-// Server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
